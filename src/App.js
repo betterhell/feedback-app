9cb8354a17e4ad366bce3f33ea63c15ea2576a0f
Dmitriy betterHell <skkogame@gmail.com>
2022-09-28 17:08:34 +0300

    Creating Feedback UI

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedBackData from "./data/FeedbackData";
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedBackData);
+
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
+  );
+}
+
+export default App;
