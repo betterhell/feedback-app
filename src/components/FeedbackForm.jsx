6877000c4e2cbe6d5799afd2573381e04c12110c
Dmitriy betterHell <skkogame@gmail.com>
2022-09-30 10:05:47 +0300

    Creating Feedback UI

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -11,7 +11,7 @@ const FeedbackForm = () => {
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState("")
 
-    const {addFeedback, feedbackEdit} = useContext(FeedbackContext)
+    const {addFeedback, feedbackEdit, updateFeedback} = useContext(FeedbackContext)
 
     useEffect(() => {
         if (feedbackEdit.edit === true) {
@@ -44,7 +44,13 @@ const FeedbackForm = () => {
                 text,
                 rating,
             }
-            addFeedback(newFeedback)
+
+            if(feedbackEdit.edit === true) {
+                updateFeedback(feedbackEdit.item.id, newFeedback)
+            } else {
+                addFeedback(newFeedback)
+            }
+
             setText("")
         }
     }
