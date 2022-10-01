5ce11595428ca0518250c00b33b5d6a2cec373d2
Dmitriy betterHell <skkogame@gmail.com>
2022-10-01 18:38:59 +0300

    Creating Feedback UI

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -17,7 +17,7 @@ export const FeedbackProvider = ({children}) => {
 
     const fetchFeedback = async () => {
         const response = await fetch(
-            `/feedback?_sort=id&_order=desc`
+            `http://localhost:5000/feedback?_sort=id&_order=desc`
         )
         const data = await response.json()
 
@@ -33,7 +33,7 @@ export const FeedbackProvider = ({children}) => {
     }
 
     const addFeedback = async (newFeedback) => {
-        const response = await  fetch(`/feedback`, {
+        const response = await  fetch("http://localhost:5000/feedback", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -47,14 +47,14 @@ export const FeedbackProvider = ({children}) => {
 
     const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure want to delete?")) {
-            await fetch(`/feedback/${id}`, { method: "DELETE"})
+            await fetch(`http://localhost:5000/feedback/${id}`, { method: "DELETE"})
 
             setFeedback(feedback.filter(item => item.id !== id))
         }
     }
 
     const updateFeedback = async (id, updItem) => {
-        const response = await fetch(`/feedback/${id}`, {
+        const response = await fetch(`http://localhost:5000/feedback/${id}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
