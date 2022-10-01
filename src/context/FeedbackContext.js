80060875c8f72628f521dc8f0abbdffe8507e61b
Dmitriy betterHell <skkogame@gmail.com>
2022-10-01 18:57:01 +0300

    Creating Feedback UI

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -16,13 +16,11 @@ export const FeedbackProvider = ({children}) => {
     }, [])
 
     const fetchFeedback = async () => {
-        const response = await fetch(
-            `/feedback?_sort=id&_order=desc`
-        )
+        const response = await fetch(`/feedback?_sort=id&_order=desc`)
         const data = await response.json()
 
         setFeedback(data)
-        setIsLoading()
+        setIsLoading(false)
     }
 
     const editFeedback = (item) => {
@@ -47,7 +45,7 @@ export const FeedbackProvider = ({children}) => {
 
     const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure want to delete?")) {
-            await fetch(`/feedback/${id}`, { method: "DELETE"})
+            await fetch(`/feedback/${id}`, {method: "DELETE"})
 
             setFeedback(feedback.filter(item => item.id !== id))
         }
